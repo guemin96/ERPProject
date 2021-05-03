840f802b46e7a298d9b1840f71ca1d0f9b96452d
guemin96 <guemin12@naver.copm>
2021-05-03 20:25:29 +0900

    초기 커밋.

---
+++ Helper/Commons.cs
@@ -0,0 +1,15 @@
+﻿using ERPProject.Model;
+using NLog;
+
+namespace ERPProject
+{
+    public class Commons
+    {
+        //NLog 정적 인스턴스 생성
+        public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        //로그인한 유저 정보
+        public static User LOGINED_USER;
+
+    }
+}
